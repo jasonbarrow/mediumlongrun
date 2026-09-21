f7667e1b46b346aa40470237cd5e10e19a0f16b1
jasonbarrow <barrow@jasonbarrow.com>
2026-09-21 16:38:05 -0400

    Remove CopyWebpackPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,6 @@
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
-import CopyWebpackPlugin from "copy-webpack-plugin";
-
 export default {
   mode: "production",
   entry: "./src/index.js",
@@ -19,10 +17,6 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-    }),
-
-    new CopyWebpackPlugin({
-      patterns: [{ from: "public", to: "." }],
     }),
   ],
   module: {
